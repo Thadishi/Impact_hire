b9a0257929e9fe67f5eb4a13e2a309b025e239d3
ThaboKopane <kpntha001@myuct.ac.za>
2019-09-04 13:36:59 +0200

    I don't know what I changed

--- Impact.java
+++ Impact.java
@@ -15,7 +15,7 @@ public class Impact implements NumberRangeSummarizer{
     public Collection<Integer> collect(String input) {
         
         //split each item from strign and convert to integer
-        //convert to a list using java streams
+        //convert string to a list using java streams
         List<Integer> tempList = Stream.of(input.split("\\,")).map(Integer::parseInt).collect(Collectors.toList());
         //conver list to arraylist because collections
         Collection<Integer> items = new ArrayList<>(tempList);
@@ -24,22 +24,29 @@ public class Impact implements NumberRangeSummarizer{
 
     public StringBuilder copy(StringBuilder stringb, int start,  int prev){
 
+        //use dstrig builder 
         stringb.append(start);
-
-        if(start!=prev) stringb.append(prev-start >1? "-":", ").append(prev);
+    
+        //if the start doesn't equal the next or previou
+        if(start!=prev)
+            stringb.append(prev-start >1? "-":", ").append(prev); //if difference is greater than 1, comma, else 
         return stringb;
     }
     
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        String summary = "";
-        
+        //where we store our strings
         StringBuilder stringb = new StringBuilder();
         
+        //convetting collection to list
         List<Integer> userInput = new ArrayList<>(input);
 
-        int prev = userInput.get(0), start = prev;
+
+        //from the beginning the previous symbol is 0, and it is also star symbol
+        int prev = userInput.get(0);
+        int start = prev;
+        //iterate throuhgh the list from indx 1, 
         for( int x : userInput.subList(1, userInput.size())){
             if(prev+1 != x){
                 copy(stringb, start, prev).append(", ");
