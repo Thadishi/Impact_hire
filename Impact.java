4cb66373c8a55c58512088687153ad72892e3207
ThaboKopane <kpntha001@myuct.ac.za>
2019-09-04 03:56:41 +0200

    stream and builders

--- Impact.java
+++ Impact.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.io.*;
 import java.util.List;
@@ -13,34 +14,34 @@ public class Impact implements NumberRangeSummarizer{
     
     @Override
     public Collection<Integer> collect(String input) {
-        Collection<Integer> items = new ArrayList<Integer>();
         
         //split each item from strign and convert to integer
-        //String[] 
-        //int[] array = Arrays.asList(input).stream().mapToInt(Integer::parseInt).toArray();
-
-
+        //convert to a list using java streams
+        List<Integer> tempList = Stream.of(input.split("\\,")).map(Integer::parseInt).collect(Collectors.toList());
+        //conver list to arraylist because collections
+        Collection<Integer> items = new ArrayList<>(tempList);
         return items;
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
-        return null;
+        String summary = "";
+        
+
+
+        return summary;
     }
 
     public static void main(String[] args){
         
         String jude = "1,3,6,7,8,12,13,14,15,21,22,23,24,31";
         
-        String[] judas = jude.split("\\,");
+        Impact thabos = new Impact();
+        Collection<Integer> fongkong = thabos.collect(jude);
 
+
+        String ranges = compress(asList(1,2,3,4,5, 12,13,14, 19)).collect(joining("-"));
         
-        int[] array = Arrays.asList(jude.split("\\,")).stream().mapToInt(Integer::parseInt).toArray();
-        List<Integer> longIds = Stream.of(jude.split("\\,")).map(Integer::parseInt).collect(Collectors.toList());
-        ArrayList<Integer> fongkong = new ArrayList<>(longIds);
-        for(int j=0; j<fongkong.size(); j++){
-            System.out.println(fongkong.get(j));
-        }
     }
 
 }
